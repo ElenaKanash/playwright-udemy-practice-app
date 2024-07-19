c16824afb959bb451b98245cf8f77e48dd166582
ElenaKanash <hird250@gmail.com>
2024-07-19 12:16:13 +0300

    add test Form Layouts page

---
+++ tests/uiElements/forms.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Form Layouts page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:4200/");
+    await page.getByText("Forms").click();
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("Input Fields", async ({ page }) => {
+    const emailInputField = page
+      .locator("nb-card", { hasText: "Using the Grid" })
+      .getByRole("textbox", { name: "Email" });
+
+    await emailInputField.fill("test@test.com");
+    await emailInputField.clear();
+    await emailInputField.pressSequentially("test2@test.com", { delay: 300 });
+
+    //generic assertion
+    const inputValue = await emailInputField.inputValue();
+    expect(inputValue).toEqual("test2@test.com");
+
+    //locator assertion
+    await expect(emailInputField).toHaveValue("test2@test.com");
+  });
+
+  
+});
