55d922c80a8a3f5f0e69163e69c771875a7657b4
ElenaKanash <hird250@gmail.com>
2024-07-23 13:52:35 +0300

    add test "Browser Dialog Box"

---
+++ tests/uiElements/browserDialogBox.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+test("Browser Dialog Box", async ({ page }) => {
+  await page.goto("http://localhost:4200/");
+  await page.getByText("Tables & Data").click();
+  await page.getByText("Smart Table").click();
+  
+  page.on('dialog', dialog => {
+    expect(dialog.message()).toEqual('Are you sure you want to delete?');
+    dialog.accept();
+  })
+  await expect(page.locator('table tbody tr').first()).toContainText('mdo@gmail.com');
+  await page.getByRole('table').locator('tr', {hasText: 'mdo@gmail.com'}).locator('.nb-trash').click();
+  await expect(page.locator('table tbody tr').first()).not.toContainText('mdo@gmail.com');
+})
