4092f601e55134bae011d18ad78d9d5506b3fbf0
ElenaKanash <hird250@gmail.com>
2024-09-04 12:46:15 +0300

    add pageManager methods  to  Form Layouts test

--- tests/formLayouts_parametrized.spec.ts
+++ tests/formLayouts_parametrized.spec.ts
@@ -1,19 +1,21 @@
 import { test, expect } from "@playwright/test";
-import { NavigationPage } from "../page_objects/navigationPage";
-import { FormLayoutsPage } from './../page_objects/formLayoutsPage';
+import { PageManager } from "../page_objects/pageManagerPage";
+/* import { NavigationPage } from "../page_objects/navigationPage";
+import { FormLayoutsPage } from './../page_objects/formLayoutsPage'; */
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
 });
 
 test("Submit Form Layouts with parametrized methods", async ({ page }) => {
-  const navigateTo = new NavigationPage(page);
-  const formLayoutsPage = new FormLayoutsPage(page);
+/*   const navigateTo = new NavigationPage(page);
+  const formLayoutsPage = new FormLayoutsPage(page); */
+  const pm = new PageManager(page);
 
-  await navigateTo.formLayoutsPage();
-  await formLayoutsPage.submitInlineFormWithParametrs('Jo Jo', 'jo@test.com', false);
-  await formLayoutsPage.submitInlineFormWithParametrs('Ivan Ivanov', 'ivanov@test.com', true);  
-  await formLayoutsPage.submitUsingTheGridFormWithParameters('test@test.com', '12345', 'Option 1');
-  await formLayoutsPage.submitUsingTheGridFormWithParameters('user@test.com', '@@@', 'Option 2');
+  await pm.navigateTo().formLayoutsPage();
+  await pm.onFormLayoutsPage().submitInlineFormWithParametrs('Jo Jo', 'jo@test.com', false);
+  await pm.onFormLayoutsPage().submitInlineFormWithParametrs('Ivan Ivanov', 'ivanov@test.com', true);  
+  await pm.onFormLayoutsPage().submitUsingTheGridFormWithParameters('test@test.com', '12345', 'Option 1');
+  await pm.onFormLayoutsPage().submitUsingTheGridFormWithParameters('user@test.com', '@@@', 'Option 2');
   
 })
