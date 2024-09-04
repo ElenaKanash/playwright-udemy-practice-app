0c7daece9df82c9bc809a8c93dbe3355ea9ac6ae
ElenaKanash <hird250@gmail.com>
2024-09-04 12:42:14 +0300

    add pageManager methods to datePicker tests

---
+++ tests/datePickers.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test";
+/* import { NavigationPage } from "../page_objects/navigationPage";
+import { DatePickerPage } from "../page_objects/datePickerPage"; */
+import { PageManager } from "../page_objects/pageManagerPage";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:4200/");
+});
+
+test("Select Common DatePicker Date From Today", async ({ page }) => {
+  const pm = new PageManager(page);
+ /*  const navigateTo = new NavigationPage(page);
+  const datePickerPage = new DatePickerPage(page); */
+
+  await pm.navigateTo().datepickerPage();
+  await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(7);
+  await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(7, 400); 
+});
